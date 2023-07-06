8072f539f2206f2e0b120b741dea00663c611b2e
m(m) <m@ex.com>
2023-07-06 23:12:57 +0300

    new test

--- lab3-test/test.cpp
+++ lab3-test/test.cpp
@@ -1,14 +1,45 @@
 #include "histogram.h"
 #include <cassert>
-void
-test_positive() {
-double min = 0;
-double max = 0;
-find_minmax({1, 2, 3}, min, max);
-assert(min == 1);
-assert(max == 3);
+
+void test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
 }
-int
-main() {
-test_positive();
+void test_n3() {
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
 }
+void test_only() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+void test_one() {
+    double min = 0;
+    double max = 0;
+    find_minmax({3}, min, max);
+    assert(min == 3);
+    assert(max == 3);
+}
+void test_no() {
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+int main() {
+    test_one();
+    test_only();
+    test_n3();
+    test_positive();
+}
+
